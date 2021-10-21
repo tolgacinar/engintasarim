dd8654507941b0249af951ba242ac60f6b0cad19
tolgacinar <tolgan23@gmail.com>
2021-10-21 14:11:22 +0300

    htaccess oluşturuldu

--- admin/controllers/Admin.php
+++ admin/controllers/Admin.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
-	public function index() {
+	public function dashboard() {
 		echo "test";
 	}
 
