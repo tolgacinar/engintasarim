dba6fd1a707c99a1e325837bb07bba1449112665
tolgacinar <tolgan23@gmail.com>
2021-10-21 13:54:58 +0300

    Modüler yapı kuruldu.

---
+++ admin/controllers/Admin.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Admin extends CI_Controller {
+
+	public function index() {
+		echo "test";
+	}
+
+}
+
+/* End of file Admin.php */
+/* Location: .//D/xampp/htdocs/engintasarim/admin/controllers/Admin.php */
--- system/application/config/config.php
+++ system/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -521,3 +521,7 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+$config['modules_locations'] = array(
+   FCPATH => '../../../',
+);
