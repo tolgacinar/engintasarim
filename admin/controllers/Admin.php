936e42cf15e35f8d75fdd33a3803d89fea33863d
tolgacinar <tolgan23@gmail.com>
2021-10-23 21:30:33 +0300

    authentication
    Giriş ve çıkışlar için sistem entegre edildi.

--- admin/controllers/Admin.php
+++ admin/controllers/Admin.php
@@ -3,6 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin extends CI_Controller {
 
+	public function __construct() {
+		parent::__construct();
+		if (!$this->ion_auth->logged_in()) {
+			redirect(base_url("admin/auth"),'refresh');
+		}
+	}
+
 	public function dashboard() {
 		$this->layout->render_admin("dashboard", []);
 	}
